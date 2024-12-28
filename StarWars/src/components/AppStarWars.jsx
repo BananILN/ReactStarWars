baa10bd46cdc9f38a89528e2c838eb180c9860fd
BananILN <daniil.klovans84@mail.ru>
2024-12-28 10:32:52 +0400

    Guard Condition

--- StarWars/src/components/AppStarWars.jsx
+++ StarWars/src/components/AppStarWars.jsx
@@ -1,72 +1,54 @@
-import {getRandomElementOfArr, getRandomHexColor} from "./utils"
+import {getRandomElementOfArr, getRandomHexColor , TIMER_INTERVAL} from "./utils"
 import { API_URL } from "./api";
 import { useEffect, useRef, useState } from "react";
 import ReactLoading from "react-loading";
 
 export default function StarWars(){
-    
+    const [loading, setLoading] = useState(true);
+    const [hero, setHero] = useState({ name: 'undefined' });
     const intervalRef = useRef(undefined);
-    const [loading, setLoading] = useState(false);
-    
-    if(intervalRef.current === undefined && !loading){
-        
-        setTimeout(()=>{
-            setLoading(true)
-        },1000)
-        return (
-            <ReactLoading type="bars" color="#0000FF"  height={100} width={50}/>
-        )  
-    }
-    return(
-        <AppStarWars />
-    )
-}
-
- function AppStarWars(){
-     const intervalRef = useRef(undefined);
-    const [hero, setHero] = useState({name: 'undefined'});
     const randColor = getRandomHexColor();
-   
 
-   
+    useEffect(() => {
+        getRandomHero();
 
-    useEffect(()=>{
-        getRandomHero()
-        console.log("Mount");
-    
+        intervalRef.current = setInterval(() => {
+            getRandomHero();
+        }, TIMER_INTERVAL);
 
-        intervalRef.current =  setInterval(()=>{
-            getRandomHero()
-        }, 3000)
-
-        return () =>{
-            console.log("unMount");
+        return () => {
             clearInterval(intervalRef.current);
-        } 
-    },[])
+        };
+    }, []);
 
     const getRandomHero = () => {
         fetch(API_URL)
             .then((response) => response.json())
-            .then((data) =>{
-                const hero  = data.results;
+            .then((data) => {
+                const hero = data.results;
                 const randHero = getRandomElementOfArr(hero);
-                setHero({name: randHero.name})
-            })
-    }
+                setHero({ name: randHero.name });
+                setLoading(false);
+            });
+    };
 
-    const getRandomHeroHandler= () =>{
+    const getRandomHeroHandler = () => {
+        clearInterval(intervalRef.current);
         getRandomHero();
+        intervalRef.current = setInterval(() => {
+            getRandomHero();
+        }, TIMER_INTERVAL);
+    };
+
+    if (loading) {
+        return <ReactLoading type="bars" color="#0000FF" height={100} width={50} />;
     }
-     
-   
-
-
 
     return (
         <>
-        <h2 className="Hero" style={{backgroundColor: randColor}}>Hero: {hero.name}</h2>
-        <button onClick={getRandomHeroHandler}>Get Hero</button>
+            <h2 className="Hero" style={{ backgroundColor: randColor }}>Hero: {hero.name}</h2>
+            <button onClick={getRandomHeroHandler}>Get Hero</button>
         </>
-    )
+    );
 }
+
--- StarWars/src/components/utils.js
+++ StarWars/src/components/utils.js
@@ -6,3 +6,5 @@ export const getRandomHexColor = () =>{
     return "#" + Math.floor(Math.random() * 16777215).toString(16)
 }
 
+
+export const TIMER_INTERVAL = 3000;
